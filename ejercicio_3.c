54656d19bc1acbe29c89939524eaa5bc6bbca130
Miranda Jesús <jesusmirdz10@gmail.com>
2026-02-26 19:18:10 -0600

    Update ejercicio_3.
    struct del circulo

--- ejercicio_3.c
+++ ejercicio_3.c
@@ -1,1 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdbool.h>
 
+#define PI 3.1416
+
+//struct para el circulo
+typedef struct {
+int x;//cordenada x del centro
+int y;//cordenada y
+float radio;
+} circulo;
