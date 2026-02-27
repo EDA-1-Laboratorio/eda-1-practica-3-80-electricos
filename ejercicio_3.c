80222aa52ea6f56d950d9f789055c5f3955cb3db
Miranda Jesús <jesusmirdz10@gmail.com>
2026-02-26 19:23:59 -0600

    Update ejercicio_3.c
    estructura para el programa general

--- ejercicio_3.c
+++ ejercicio_3.c
@@ -10,3 +10,25 @@ int x;//cordenada x del centro
 int y;//cordenada y
 float radio;
 } circulo;
+
+//fucion para hacer un nuevo circulo 
+
+
+
+//funcion para calcular la distancia entre centro
+
+
+
+//funcion para ver si se intersectan 
+
+
+
+// >Opcional< Funcion para calcular el area de interseccion
+
+
+
+//Funcion para la entrada de datos para el circulo
+
+
+
+//main
